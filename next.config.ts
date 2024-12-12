5db1abe572fe63a798fc0196263ceb8c9d3b9988
01hamza97 <hamza1.00074@gmail.com>
2024-12-12 22:11:38 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,52 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	// output: 'standalone',
+  /* config options here */
+  // output: 'standalone',
 
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
-	sassOptions: {
-		silenceDeprecations: ['legacy-js-api'],
-	},
-	experimental: {
-
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'monstro-bucket.s3.us-east-2.amazonaws.com',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'mymonstroapp.com',
-			}
-		]
-	}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
+  experimental: {
+    largePageDataBytes: 128 * 1024, // 128KB
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'monstro-bucket.s3.us-east-2.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mymonstroapp.com',
+      }
+    ]
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+    responseLimit: '8mb',
+    externalResolver: true,
+  },
+  serverRuntimeConfig: {
+    maxHeaderSize: 32 * 1024, // 32KB
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/protected/:path*',
+        destination: 'https://mymonstroapp.com/api/protected/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
