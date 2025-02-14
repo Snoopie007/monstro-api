094b607101d6a8fca96a7e8e94911bcfa461b487
Snoopie007 <steve00006@gmail.com>
2025-02-14 12:03:03 -0500

    Update

--- src/app/dashboard/[id]/members/[mid]/components/MemberEnrollments/create-enrollment.tsx
+++ src/app/dashboard/[id]/members/[mid]/components/MemberEnrollments/create-enrollment.tsx
@@ -111,8 +111,7 @@ export function CreateEnrollment() {
                                                 </FormControl>
                                                 <SelectContent>
                                                     <SelectItem value="m@example.com">m@example.com</SelectItem>
-                                                    <SelectItem value="m@google.com">m@google.com</SelectItem>
-                                                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+
                                                 </SelectContent>
                                             </Select>
 
