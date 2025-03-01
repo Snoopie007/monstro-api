29da3c2ad788031e676a0d47773a8783d7cd2c4d
Snoopie007 <steve00006@gmail.com>
2025-02-28 22:13:33 -0500

    small update

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -118,7 +118,7 @@ function getCurrentStatus(sessionData: any, userTimezone: any) {
 	}
 
 	const { session } = sessionData;
-	console.log(session?.level?.program.location.timezone)
+
 	const locationTimezone = session?.level?.program.location.timezone || 'UTC';
 
 	// Convert session start and end dates to the program's timezone
@@ -126,70 +126,65 @@ function getCurrentStatus(sessionData: any, userTimezone: any) {
 	const endMoment = dayjs.tz(session.endDate, locationTimezone);
 	const currentTime = dayjs.tz(new Date(), locationTimezone);
 
-	if (currentTime.isBefore(startMoment)) {
-			return "Session Not Started";
-	} else if (currentTime.isAfter(endMoment)) {
-			return "Session Ended";
-	} else {
-			const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-			const currentDayOfWeek = currentTime.format('dddd').toLowerCase();
-			let nextClassDayIndex = daysOfWeek.indexOf(currentDayOfWeek);
-			let daysToAdd = 0;
+	if (currentTime.isBefore(startMoment)) return "Session Not Started";
 
-			// Find the next available session day
-			for (let i = 0; i <= 7; i++) {
-					let nextDayIndex = (nextClassDayIndex + i) % 7;
-					let nextDay = daysOfWeek[nextDayIndex];
+	if (currentTime.isAfter(endMoment)) return "Session Ended";
 
-					if (session[nextDay]) {
-							daysToAdd = i;
-							break;
-					}
-			}
+	const daysOfWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+	const currentDayOfWeek = currentTime.format('dddd').toLowerCase();
+	let nextClassDayIndex = daysOfWeek.indexOf(currentDayOfWeek);
+	let daysToAdd = 0;
 
-			let nextSessionDate = currentTime.add(daysToAdd, 'days');
-			let nextSessionDay = nextSessionDate.format('dddd').toLowerCase();
+	// Find the next available session day
+	for (let i = 0; i <= 7; i++) {
+			let nextDayIndex = (nextClassDayIndex + i) % 7;
+			let nextDay = daysOfWeek[nextDayIndex];
 
-			// Get start time for the next session
-			let startTime = dayjs.tz(`${nextSessionDate.format('YYYY-MM-DD')} ${session[nextSessionDay]}`, 'YYYY-MM-DD HH:mm:ss', locationTimezone);
-
-			let duration = JSON.parse(session.duration_time || '{}');
-			let sessionDuration = duration[nextSessionDay] || 0;
-
-			let endTime = startTime.add(sessionDuration, 'minutes');
-
-			// If the session is currently in progress
-			if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
-					return 'In Progress';
-			}
-
-			// Calculate time until next session
-			let timeUntilNextSession = dayjs.duration(startTime.diff(currentTime));
-			let hours = timeUntilNextSession.hours() + (timeUntilNextSession.days() * 24);
-			let minutes = timeUntilNextSession.minutes();
-			let days = timeUntilNextSession.days();
-
-			// Convert time to user's timezone
-			let convertedTime = startTime.tz(userTimezone).format('h:mm A');
-
-			if (hours < 24 && days === 0) {
-					return hours === 0 ? `Next session starts in ${minutes} minutes.` : `Next session starts in ${hours} hours and ${minutes} minutes.`;
-			} else if (days < 2) {
-					return `Next session starts tomorrow at ${convertedTime}`;
-			} else {
-					return `Next session starts on ${nextSessionDay} at ${convertedTime}`;
+			if (session[nextDay]) {
+					daysToAdd = i;
+					break;
 			}
 	}
+
+	let nextSessionDate = currentTime.add(daysToAdd, 'days');
+	let nextSessionDay = nextSessionDate.format('dddd').toLowerCase();
+
+	// Get start time for the next session
+	let startTime = dayjs.tz(`${nextSessionDate.format('YYYY-MM-DD')} ${session[nextSessionDay]}`, 'YYYY-MM-DD HH:mm:ss', locationTimezone);
+
+	let duration = JSON.parse(session.duration_time || '{}');
+	let sessionDuration = duration[nextSessionDay] || 0;
+
+	let endTime = startTime.add(sessionDuration, 'minutes');
+
+	// If the session is currently in progress
+	if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
+			return 'In Progress';
+	}
+
+	// Calculate time until next session
+	let timeUntilNextSession = dayjs.duration(startTime.diff(currentTime));
+	let hours = timeUntilNextSession.hours() + (timeUntilNextSession.days() * 24);
+	let minutes = timeUntilNextSession.minutes();
+	let days = timeUntilNextSession.days();
+
+	// Convert time to user's timezone
+	let convertedTime = startTime.tz(userTimezone).format('h:mm A');
+
+	if (hours < 24 && days === 0) return hours === 0 ? `Next session starts in ${minutes} minutes.` : `Next session starts in ${hours} hours and ${minutes} minutes.`;
+
+	if (days < 2) return `Next session starts tomorrow at ${convertedTime}`;
+
+	return `Next session starts on ${nextSessionDay} at ${convertedTime}`;
+	
 }
 
 export {
-  
-  convertToCurrency,
-  sleep,
-  tryCatch,
-  formatAmountForDisplay,
- 
-  cn,
+	convertToCurrency,
+	sleep,
+	tryCatch,
+	formatAmountForDisplay,
+  	cn,
 	decodeJWT,
 	getCurrentTimeDetails,
 	getCurrentStatus
