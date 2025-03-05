af7dce8e2979a177dedb1ad4015741ae1e0e0901
Snoopie007 <steve00006@gmail.com>
2025-03-04 23:41:59 -0500

    updates

--- src/app/api/protected/[id]/members/[mid]/attendances/route.ts
+++ src/app/api/protected/[id]/members/[mid]/attendances/route.ts
@@ -1,34 +1,31 @@
 import { NextResponse } from 'next/server';
-import { auth } from "@/auth";
+
 import { db } from '@/db/db';
-import { cp } from 'fs';
 
 export async function GET(req: Request, props: { params: Promise<{ mid: number, id: number }> }) {
     const params = await props.params;
     try {
-        const session = await auth();
-        if (session) {
-            const enrollments = await db.query.reservations.findMany({
-                where: (reservations, { eq }) => eq(reservations.memberId, params.mid),
-                with: {
-                    attendances: true,
-                    session: {
-                        with: {
-                            program: true
-                        }
+
+        const reservations = await db.query.reservations.findMany({
+            where: (reservations, { eq }) => eq(reservations.memberId, params.mid),
+            with: {
+                attendances: true,
+                session: {
+                    with: {
+                        program: true
                     }
                 }
-            });
+            }
+        });
 
-            const attendances = enrollments.flatMap(e =>
-                e.attendances.map(att => ({
-                    ...att,
-                    program: e.session?.program || null
-                }))
-            );
+        const attendances = reservations.flatMap(e =>
+            e.attendances.map(att => ({
+                ...att,
+                program: e.session?.program || null
+            }))
+        );
 
-            return NextResponse.json(attendances, { status: 200 });
-        }
+        return NextResponse.json(attendances, { status: 200 });
     } catch (err) {
         console.log(err)
         return NextResponse.json({ error: err }, { status: 500 })
