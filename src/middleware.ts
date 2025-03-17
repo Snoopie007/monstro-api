139f1aa9597a9cea5dc41a36679c682ae823a462
Snoopie007 <steve00006@gmail.com>
2025-03-17 12:09:33 -0400

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -71,18 +71,25 @@ export default auth(async (req) => {
 
 		// Handle home page and dashboard redirects (only for web, not for mobile)
 		if (isLoggedin && !isMobileApp) {
-			console.log(pathname)
+
+
 			if (pathname.startsWith("/api/")) {
 				return NextResponse.next()
 			}
+
 			if (locations.length === 0 && pathname !== "/onboarding") {
 				return NextResponse.redirect(new URL("/onboarding", req.nextUrl.origin))
 			}
 
+			if (pathname === "/") {
+				const next = locations.find((loc: { status: string }) => loc.status === "active") || locations.find((loc: { status: string }) => loc.status === "incomplete")
+				return NextResponse.redirect(new URL(`/${next.status === "active" ? "dashboard" : "onboarding"}/${next.id}`, req.nextUrl.origin))
+			}
+
+
 			const [, path, locationId] = pathname.match(/^\/((?:dashboard|onboarding))\/([^/]+)(\/.*)?$/) || []
 
 			if (locationId) {
-
 				/** Check if locationId is a valid location */
 				let nextLocation = locations.find((loc: { id: string }) => loc.id === locationId)
 
