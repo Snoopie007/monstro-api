02d9c6de85985bee052474f16ce6398e94ef0a16
Snoopie007 <steve00006@gmail.com>
2025-03-19 16:39:37 -0400

    update 2

--- src/libs/server/MDXParse.ts
+++ src/libs/server/MDXParse.ts
@@ -12,7 +12,7 @@ const GitHubUser = "Snoopie007"
 const Repo = "monstro-blog"
 
 export async function getTOS(fileName: string, folder: string = "pages"): Promise<MonstroLegal | undefined> {
-    console.log(fileName, folder)
+
     const res = await fetch(`https://raw.githubusercontent.com/${GitHubUser}/${Repo}/main/${folder}/${fileName}.mdx`, {
         headers: {
             Accept: 'application/vnd.github+json',
