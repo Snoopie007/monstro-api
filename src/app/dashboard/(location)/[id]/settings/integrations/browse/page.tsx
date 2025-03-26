b7f2a2f18dbe4210619f50f211c5b3e670235cbf
01hamza97 <hamza1.00074@gmail.com>
2025-03-26 15:45:12 +0500

    GHL

--- src/app/dashboard/(location)/[id]/settings/integrations/browse/page.tsx
+++ src/app/dashboard/(location)/[id]/settings/integrations/browse/page.tsx
@@ -1,40 +1,29 @@
 'use client'
 import Image from "next/image"
-
-
 import Link from "next/link";
 import { Partner } from "@/types";
 import { ChangeEvent, useState, use } from "react";
 import { IntergrationPartners } from './partners';
 import { Badge, Button } from "@/components/ui";
 
-
-export default function BrowseIntergrationPage(props: { params: Promise<{ id: string }> }) {
-
+export default function BrowseIntegrationPage(props: { params: Promise<{ id: string }> }) {
     const params = use(props.params);
     const [filteredPartners, setFilteredPartners] = useState<Partner[]>(IntergrationPartners);
-
 
     function filterPartner(event: ChangeEvent<HTMLInputElement>): void {
         const query = event.target.value.toLowerCase();
         if (query !== '') {
             const filtered = IntergrationPartners.filter(partner => partner.name.toLowerCase().includes(query));
-            console.log(filtered)
             setFilteredPartners(filtered);
-
         } else {
             setFilteredPartners(IntergrationPartners);
         }
-
     }
 
     function generateURL(partner: Partner) {
-
         const queries = Object.entries(partner.options)
             .map(([key, value]) => {
-                // Check if the value is an array
                 if (Array.isArray(value)) {
-                    // Join the array elements with a space
                     value = value.join(' ');
                 }
                 if (key == "state") {
@@ -42,36 +31,31 @@ export default function BrowseIntergrationPage(props: { params: Promise<{ id: st
                 }
                 return `${key}=${value}`;
             }).join('&');
-        return `${partner.url}?${queries}`;
-        // const url = `https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=${options.requestType}&redirect_uri=${options.redirectUri}&client_id=${options.clientId}&scope=${options.scopes.join(' ')}`;
-        // window.open(url, 'PopupWindow', 'width=600,height=600,scrollbars=yes');
-        // window.success = (data: any) => {
-        //     console.log("data")
-        // }
 
+        return `${partner.url}?${queries}`;
     }
 
     return (
         <div className="space-y-4">
-            <div >
-                <input placeholder='Search Intergrations' className='flex-1 w-full rounded-xs text-sm bg-transparent py-1.5 px-4 border-foreground/60 border ' />
+            <div>
+                <input
+                    placeholder='Search Integrations'
+                    className='flex-1 w-full rounded-xs text-sm bg-transparent py-1.5 px-4 border-foreground/60 border'
+                    onChange={filterPartner}
+                />
             </div>
             <div className="grid grid-cols-2 gap-4">
                 {filteredPartners.map((partner, index) => (
                     <Link key={index} href={generateURL(partner)} className="border border-foreground/20 rounded-sm py-6 px-4 space-y-4">
                         <div className="flex flex-row gap-4 items-center justify-center">
-                            <div className="">
-                                <div className="rounded-full overflow-hidden w-[45px] h-[45px] flex ">
-                                    <Image src={`/images/partners/${partner.logo}`} alt={partner.name} width={45} height={45} />
-                                </div>
+                            <div className="rounded-full overflow-hidden w-[45px] h-[45px] flex">
+                                <Image src={`/images/partners/${partner.logo}`} alt={partner.name} width={45} height={45} />
                             </div>
-
                             <div className="text-base font-bold flex-1 text-foreground text-left">
                                 {partner.name}
                                 <div className="text-xs">
                                     {partner.tags.map((tag, i) => (
                                         <Badge key={i} className="bg-indigo-500 rounded-xs">{tag}</Badge>
-
                                     ))}
                                 </div>
                             </div>
@@ -83,6 +67,6 @@ export default function BrowseIntergrationPage(props: { params: Promise<{ id: st
                     </Link>
                 ))}
             </div>
-        </div >
-    )
+        </div>
+    );
 }
--- src/app/dashboard/(location)/[id]/settings/integrations/integrations.tsx
+++ src/app/dashboard/(location)/[id]/settings/integrations/integrations.tsx
@@ -15,6 +15,7 @@ interface IntergrationProps {
 export default function IntegrationList({ integrations, locationId }: IntergrationProps) {
     const [loading, setLoading] = useState<boolean>(false);
     const { mutate } = useSWR(`/api/protected/loc/${locationId}/integrations/`);
+
     const disconnect = async (iId: number) => {
         setLoading(true);
         try {
@@ -23,7 +24,7 @@ export default function IntegrationList({ integrations, locationId }: Intergrati
                     method: 'DELETE',
                     body: JSON.stringify({ id: iId })
                 })
-            )
+            );
             if (error || !result || !result.ok) throw error;
             await mutate();
             setLoading(false);
@@ -31,12 +32,10 @@ export default function IntegrationList({ integrations, locationId }: Intergrati
             toast.error(error);
             setLoading(false);
         }
-    }
+    };
 
     if (integrations.length === 0) {
-        return (
-            <div className='text-center'> No intergrations found.</div>
-        )
+        return <div className='text-center'>No integrations found.</div>;
     }
 
     return (
@@ -44,38 +43,27 @@ export default function IntegrationList({ integrations, locationId }: Intergrati
             {integrations.map((integration, index) => (
                 <li key={index} className="bg-background flex flex-row gap-2 items-center w-full min-h-4 py-2 px-6 rounded-[4px] border ">
                     <div className='flex-1 flex flex-row gap-4 items-center'>
-                        <div className='flex-1 flex flex-row gap-4 items-center'>
-                            <div className="rounded-sm overflow-hidden">
-                                <Image src={`/images/partners/${integration.service.toLowerCase()}-logo.png`} alt={integration.service} width={30} height={30} />
-                            </div>
-                            <div>
-                                <div className="font-medium text-sm ">{integration.service}</div>
-                            </div>
+                        <div className="rounded-sm overflow-hidden">
+                            <Image src={`/images/partners/${integration.service.toLowerCase()}-logo.png`} alt={integration.service} width={30} height={30} />
+                        </div>
+                        <div>
+                            <div className="font-medium text-sm">{integration.service}</div>
                         </div>
                     </div>
                     <div>
                         <Button
                             onClick={() => disconnect(integration.id)}
-                            variant={"ghost"}
-                            size={"sm"}
-                            className={cn("")}
+                            variant="ghost"
+                            size="sm"
                             type="button"
                         >
-                            <LucideLoader2 size={"16"} className={cn('animate-spin hidden',
-                                { 'inline-block': loading })} />
-
-
-                            <Trash2 size={'16'} className={cn('hidden stroke-red-500', {
-                                'inline-block': !loading,
-
-                            })} />
+                            <LucideLoader2 size={"16"} className={cn('animate-spin hidden', { 'inline-block': loading })} />
+                            <Trash2 size={'16'} className={cn('hidden stroke-red-500', { 'inline-block': !loading })} />
                         </Button>
                     </div>
                 </li>
-            ))
-            }
-        </ul >
-
-
-    )
+            ))}
+        </ul>
+    );
 }
+
