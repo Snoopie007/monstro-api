8647df46a590bc2f18de621e391926d02541b25e
Iliyan Faysal Vallinai <89943596+AnonymousHaw@users.noreply.github.com>
2025-04-09 19:53:20 +0500

    first commit

---
+++ src/app/api/protected/mobile/member/programs/[lid]/route.ts
@@ -0,0 +1,29 @@
+
+import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/db/db';
+import { authenticateMember } from '../../utils';
+
+export async function GET(req: NextRequest,props: { params: Promise<{ id: number, lid: number }> }) {
+  try {
+    const params = await props.params;
+
+    const authMember = authenticateMember(req);
+
+    const member = await db.query.members.findFirst({
+      where: (members, { eq }) => eq(members.id, Number(authMember.member?.id)),
+    });
+
+    if (!member) {
+      return NextResponse.json({ error: "Member not found" }, { status: 404 });
+    }
+
+    const programs = await db.query.programs.findMany({
+      where: (program, { eq }) =>
+        eq(program.locationId, Number(params.lid)), 
+    });
+
+    return NextResponse.json(programs, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 })
+  }
+}
