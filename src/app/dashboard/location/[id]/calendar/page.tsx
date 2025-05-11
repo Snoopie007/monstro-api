147cb7a28f9df2c6feeed5ca3d0ee791ac1fb602
Snoopie007 <steve00006@gmail.com>
2025-05-10 21:09:19 -0400

    update calendars 4

--- src/app/dashboard/location/[id]/calendar/page.tsx
+++ src/app/dashboard/location/[id]/calendar/page.tsx
@@ -16,63 +16,63 @@ function CalendarPage(props: { params: Promise<{ id: string }> }) {
 	const { currentDate, setCurrentDate, currentMonth, setCurrentMonth, isLoading, setIsLoading } = useSessionCalendar()
 	const [events, setEvents] = useState<CalendarEvent[]>([])
 
-	useEffect(() => {
-		const newMonth = currentDate.getMonth();
-		if (currentMonth === null || currentMonth !== newMonth) {
-			setCurrentMonth(newMonth);
-			fetchEvents();
-		}
-	}, [currentDate]);
+	// useEffect(() => {
+	// 	const newMonth = currentDate.getMonth();
+	// 	if (currentMonth === null || currentMonth !== newMonth) {
+	// 		setCurrentMonth(newMonth);
+	// 		fetchEvents();
+	// 	}
+	// }, [currentDate]);
 
 
-	async function fetchEvents() {
-		setIsLoading(true)
-		const { result, error } = await tryCatch(
-			fetch(`/api/protected/loc/${id}/events?date=${currentDate.toISOString()}`)
-		)
+	// async function fetchEvents() {
+	// 	setIsLoading(true)
+	// 	const { result, error } = await tryCatch(
+	// 		fetch(`/api/protected/loc/${id}/events?date=${currentDate.toISOString()}`)
+	// 	)
 
-		if (error || !result || !result.ok) {
-			setIsLoading(false)
-			return
-		}
+	// 	if (error || !result || !result.ok) {
+	// 		setIsLoading(false)
+	// 		return
+	// 	}
 
-		const data = await result.json()
-		setEvents(data)
-		setIsLoading(false)
-	}
+	// 	const data = await result.json()
+	// 	setEvents([])
+	// 	setIsLoading(false)
+}
 
 
-	return (
-		<div className="flex flex-row h-full bg-foreground/5">
+return (
+	<div className="flex flex-row h-full bg-foreground/5">
 
-			<div className="flex-1 h-full">
-				<BigCalendar events={events} />
-			</div>
-			<div className="flex-initial w-[300px] flex flex-col pl-0 pr-2 pt-2 pb-1 space-y-2">
+		<div className="flex-1 h-full">
+			<BigCalendar events={events} />
+		</div>
+		<div className="flex-initial w-[300px] flex flex-col pl-0 pr-2 pt-2 pb-1 space-y-2">
 
-				<Calendar
-					mode="single"
-					fromDate={new Date()}
-					selected={currentDate}
-					onSelect={(date) => {
-						if (date) {
-							setCurrentDate(date)
-						}
-					}}
-					className="rounded-lg border border-foreground/10 bg-background"
-				/>
-				<DayList lid={id} events={events} />
+			<Calendar
+				mode="single"
+				fromDate={new Date()}
+				selected={currentDate}
+				onSelect={(date) => {
+					if (date) {
+						setCurrentDate(date)
+					}
+				}}
+				className="rounded-lg border border-foreground/10 bg-background"
+			/>
+			<DayList lid={id} events={events} />
 
-			</div>
-			{/* {isSidebarOpen && selectedSession && (
+		</div>
+		{/* {isSidebarOpen && selectedSession && (
 				<Sidebar
 					session={{ ...selectedSession, locationId }}
 					reservations={reservations}
 					onClose={handleSidebarClose}
 				/>
 			)} */}
-		</div>
-	);
+	</div>
+);
 };
 
 export default CalendarPage;
