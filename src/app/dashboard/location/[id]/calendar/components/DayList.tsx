36ae60023016aec9df92eae70412821c0e85275a
Snoopie007 <steve00006@gmail.com>
2025-05-11 21:04:52 -0400

    fix recurring 4

--- src/app/dashboard/location/[id]/calendar/components/DayList.tsx
+++ src/app/dashboard/location/[id]/calendar/components/DayList.tsx
@@ -3,6 +3,7 @@ import { useSessionCalendar } from '../providers/SessionCalendarProvider'
 import { format, isSameDay } from 'date-fns'
 import { CalendarEvent } from '@/types/attendance'
 import { ScrollArea } from '@/components/ui'
+import { cn } from '@/libs/utils'
 
 
 
@@ -18,21 +19,31 @@ export function DayList({ lid, events }: { lid: string, events: CalendarEvent[] 
 
             setClasses(classes)
         }
+
     }, [currentDate])
+
+
 
     return (
         <div className='bg-background rounded-lg border border-foreground/10 flex-1 h-full' >
             <div className='flex flex-col flex-1 h-full p-4 space-y-2'>
-                <div className='text-base font-semibold   '>
-                    Classes for {currentDate && format(currentDate, 'MMMM d, yyyy')}
+                <div className='text-sm font-semibold  border-b border-foreground/10 pb-1'>
+                    {currentDate ? `Classes for ${format(currentDate, 'MMMM d, yyyy')}` : 'Loading...'}
                 </div>
 
                 <ScrollArea className='h-full'>
                     <div className='flex flex-col gap-2'>
                         {classes.map((c) => (
-                            <div key={c.id} className='border border-foreground/10 bg-foreground/5 rounded-sm px-4 py-3'>
-                                <div className='text-sm font-medium'>{c.title}</div>
-                                <div className='text-xs text-foreground/50'>{format(c.start, 'h:mm a')} - {format(c.end, 'h:mm a')}</div>
+                            <div key={c.id} className={cn(
+                                'border border-foreground/10 bg-foreground/5  text-xs',
+                                'rounded-sm px-3 py-2 flex  flex-col'
+                            )}>
+                                <div className=' font-bold'>
+                                    {c.title}
+                                </div>
+                                <div className='text-muted-foreground'>
+                                    {format(c.start, 'h:mm a')} - {format(c.end, 'h:mm a')}
+                                </div>
                             </div>
                         ))}
                     </div>
