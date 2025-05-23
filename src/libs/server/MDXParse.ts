01233fbe38f762a1192792b93278cb1ca7a6d2d1
Snoopie007 <steve00006@gmail.com>
2025-05-23 19:52:56 -0400

    update

--- src/libs/server/MDXParse.ts
+++ src/libs/server/MDXParse.ts
@@ -1,11 +1,6 @@
-
-import { SupportDoc, SupportDocMeta } from '@/types/admin'
-import { DocToC } from '@/types/admin'
 import { compileMDX } from 'next-mdx-remote/rsc'
 import { JSXElementConstructor } from 'react'
 import { ReactElement } from 'react'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
 
 export type MonstroLegal = {
     content: ReactElement<any, string | JSXElementConstructor<any>>,
@@ -73,72 +68,5 @@ export async function getTOS(fileName: string, folder: string = "pages"): Promis
     }
 
     return tos
-}
-
-
-function buildToC(raw: string): DocToC[] {
-    const headingsRegex = /\n(?<flag>#{1,6})\s+(?<headline>.+)/g;
-    return Array.from(raw.matchAll(headingsRegex)).map(({ groups }) => {
-        const flag = groups?.flag;
-        const headline = groups?.headline;
-        return {
-            level: flag?.length,
-            headline: headline,
-            slug: headline?.toLocaleLowerCase().replace(/[^\w\s]/gi, '').split(" ").join("-")
-        };
-    })
-}
-
-export async function getSupportDocByName(fileName: string): Promise<SupportDoc | undefined> {
-
-    const res = await fetch(`https://raw.githubusercontent.com/${GitHubUser}/support-mdx/main/${fileName}.mdx`, {
-        headers: {
-            Accept: 'application/vnd.github+json',
-            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-    });
-
-    if (!res.ok) return undefined
-
-    const rawMDX = await res.text()
-
-    if (rawMDX === '404: Not Found') return undefined
-    const duration = readingDuration(rawMDX, {
-        emoji: false
-    });
-
-    const { frontmatter, content } = await compileMDX<SupportDocMeta>({
-        source: rawMDX,
-        components: {
-
-        },
-        options: {
-            parseFrontmatter: true,
-            mdxOptions: {
-                rehypePlugins: [
-                    rehypeSlug,
-                    [rehypeAutolinkHeadings, {
-                        behavior: 'before'
-                    }],
-                ],
-            },
-        }
-    })
-
-
-    const doc: SupportDoc = {
-        toc: buildToC(rawMDX),
-        meta: {
-            title: frontmatter.title,
-            file: fileName,
-            description: frontmatter.description,
-            published: frontmatter.published,
-            duration: duration,
-            tags: frontmatter.tags
-        }, content
-    }
-
-    return doc
 }
 
