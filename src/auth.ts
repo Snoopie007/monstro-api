c1ea7fd66df6f1ee1a219ba228594d8ebd547ab2
Snoopie007 <steve00006@gmail.com>
2025-06-24 15:20:09 -0400

    update 2

--- src/auth.ts
+++ src/auth.ts
@@ -28,6 +28,7 @@ import { ExtendedUser } from "./types/next-auth";
 
 const isProduction = process.env.NODE_ENV === "production";
 
+console.log(process.env.NODE_ENV)
 export const {
 	handlers: { GET, POST },
 	auth,
