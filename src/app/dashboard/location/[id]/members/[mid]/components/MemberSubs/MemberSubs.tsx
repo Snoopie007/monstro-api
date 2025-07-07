c02a01f02e264c2524158458d1d8c5f4e47bb3f7
cosmiclasagnadev <allenpdl75@gmail.com>
2025-07-07 22:18:32 +0800

    fix for cancelling sub with "end of period" value selected

--- src/app/dashboard/location/[id]/members/[mid]/components/MemberSubs/MemberSubs.tsx
+++ src/app/dashboard/location/[id]/members/[mid]/components/MemberSubs/MemberSubs.tsx
@@ -1,126 +1,134 @@
-'use client'
+"use client";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-    CircleProgress
-} from '@/components/ui'
-import { Input } from '@/components/forms'
-import { formatAmountForDisplay } from '@/libs/utils'
-import { MemberSubscription } from '@/types'
-import { useMemberStatus } from '../../providers'
-import { SubscriptionStatus, CreateSubscription, SubActions } from '.'
-import { format } from 'date-fns'
-import { useState } from 'react'
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  CircleProgress,
+} from "@/components/ui";
+import { Input } from "@/components/forms";
+import { formatAmountForDisplay } from "@/libs/utils";
+import { MemberSubscription } from "@/types";
+import { useMemberStatus } from "../../providers";
+import { SubscriptionStatus, CreateSubscription, SubActions } from ".";
+import { format } from "date-fns";
+import { useState } from "react";
 
 function calculateProgress(start: Date, end: Date) {
-    const now = Date.now()
-    const startDate = new Date(start)
-    const endDate = new Date(end)
-    const total = endDate.getTime() - startDate.getTime()
-    const elapsed = now - startDate.getTime()
-    const progress = (elapsed / total) * 100
+  const now = Date.now();
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  const total = endDate.getTime() - startDate.getTime();
+  const elapsed = now - startDate.getTime();
+  const progress = (elapsed / total) * 100;
 
-    return Math.min(Math.max(Number(progress.toFixed(2)), 10), 100) // Set minimum to 10%
+  return Math.min(Math.max(Number(progress.toFixed(2)), 10), 100); // Set minimum to 10%
 }
 
 const RowHeaders = [
-    'Plan',
-    'Duration',
-    'Amount',
-    'Total Collected',
-    'Payment Type',
-    'Next Invoice',
-    'Status',
-]
+  "Plan",
+  "Duration",
+  "Amount",
+  "Total Collected",
+  "Payment Type",
+  "Next Invoice",
+  "Status",
+];
 
-export function MemberSubs({ params }: { params: { id: string, mid: string }, }) {
-    const { member } = useMemberStatus();
-    const [search, setSearch] = useState<string>('');
+export function MemberSubs({
+  params,
+}: {
+  params: { id: string; mid: string };
+}) {
+  const { member } = useMemberStatus();
+  const [search, setSearch] = useState<string>("");
 
-    return (
-        <div className='space-y-0'>
-            <div className='w-full flex flex-row items-center px-4 py-2  bg-foreground/5  gap-2'>
-                <Input placeholder='Search subs...' className='w-auto bg-background border-foreground/10 h-9' />
-                <CreateSubscription params={params} />
-            </div>
-            <div className='border-y border-foreground/10'>
-                <Table className=''>
-                    <TableHeader>
-                        <TableRow >
-                            {RowHeaders.map((header, i) => (
-
-                                <TableHead key={i} className='text-sm font-normal h-auto  py-2'>{header}</TableHead>
-
-                            ))}
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        <SubscriptionRow subscriptions={member?.subscriptions || []} />
-                    </TableBody>
-                </Table>
-            </div>
-        </div >
-    )
+  return (
+    <div className="space-y-0">
+      <div className="w-full flex flex-row items-center px-4 py-2  bg-foreground/5  gap-2">
+        <Input
+          placeholder="Search subs..."
+          className="w-auto bg-background border-foreground/10 h-9"
+        />
+        <CreateSubscription params={params} />
+      </div>
+      <div className="border-y border-foreground/10">
+        <Table className="">
+          <TableHeader>
+            <TableRow>
+              {RowHeaders.map((header, i) => (
+                <TableHead key={i} className="text-sm font-normal h-auto  py-2">
+                  {header}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <SubscriptionRow subscriptions={member?.subscriptions || []} />
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
 
-function SubscriptionRow({ subscriptions }: { subscriptions: MemberSubscription[] }) {
-
-    if (subscriptions.length < 1) {
-        return (
-            <TableRow>
-                <TableCell colSpan={6} className='text-center'>
-                    <p className='text-sm text-gray-500'>No subscriptions found</p>
-                </TableCell>
-            </TableRow>
-        )
-    }
-
+function SubscriptionRow({
+  subscriptions,
+}: {
+  subscriptions: MemberSubscription[];
+}) {
+  if (subscriptions.length < 1) {
     return (
+      <TableRow>
+        <TableCell colSpan={6} className="text-center">
+          <p className="text-sm text-gray-500">No subscriptions found</p>
+        </TableCell>
+      </TableRow>
+    );
+  }
 
-        <>
-            {subscriptions.map((sub: MemberSubscription) => (
-                <TableRow key={sub.id} className='group '>
-
-                    <TableCell>
-                        <div className='flex flex-row items-center justify-between'>
-                            <div className='flex flex-row gap-2 items-center'>
-                                <div className="relative flex items-center justify-center w-5 h-5">
-                                    <CircleProgress progress={calculateProgress(sub.currentPeriodStart, sub.currentPeriodEnd)} />
-                                </div>
-                                <span>{sub.plan?.name}</span>
-                            </div>
-                            <SubActions sub={sub} />
-                        </div>
-                    </TableCell>
-                    <TableCell>
-                        {format(sub.startDate, "MMM d, yyyy")} - {sub.endedAt ? format(sub.endedAt, "MMM d, yyyy") : 'Never'}
-                    </TableCell>
-                    <TableCell>
-                        {formatAmountForDisplay(sub.plan?.price! / 100, 'USD', true)} / {sub.plan?.interval}
-                    </TableCell>
-                    <TableCell>
-
-                    </TableCell>
-                    <TableCell className='uppercase'>
-                        {sub.paymentMethod}
-                    </TableCell>
-                    <TableCell>
-                        {(sub.status !== 'active' || sub.cancelAt) ? (
-                            "No future invoices"
-                        ) : (
-                            format(sub.currentPeriodEnd, "MMM d, yyyy")
-                        )}
-                    </TableCell>
-                    <TableCell>
-                        <SubscriptionStatus sub={sub} />
-                    </TableCell>
-
-                </TableRow>
-            ))}
-        </>
-    )
+  return (
+    <>
+      {subscriptions.map((sub: MemberSubscription) => (
+        <TableRow key={sub.id} className="group ">
+          <TableCell>
+            <div className="flex flex-row items-center justify-between">
+              <div className="flex flex-row gap-2 items-center">
+                <div className="relative flex items-center justify-center w-5 h-5">
+                  <CircleProgress
+                    progress={calculateProgress(
+                      sub.currentPeriodStart,
+                      sub.currentPeriodEnd
+                    )}
+                  />
+                </div>
+                <span>{sub.plan?.name}</span>
+              </div>
+              <SubActions sub={sub} />
+            </div>
+          </TableCell>
+          <TableCell>
+            {format(sub.startDate, "MMM d, yyyy")} -{" "}
+            {sub.endedAt ? format(sub.endedAt, "MMM d, yyyy") : "Never"}
+          </TableCell>
+          <TableCell>
+            {formatAmountForDisplay(sub.plan?.price! / 100, "USD", true)} /{" "}
+            {sub.plan?.interval}
+          </TableCell>
+          <TableCell></TableCell>
+          <TableCell className="uppercase">{sub.paymentMethod}</TableCell>
+          <TableCell>
+            {sub.status !== "active" || sub.cancelAt
+              ? "No future invoices"
+              : format(sub.currentPeriodEnd, "MMM d, yyyy")}
+          </TableCell>
+          <TableCell>
+            <SubscriptionStatus sub={sub} />
+          </TableCell>
+        </TableRow>
+      ))}
+    </>
+  );
 }
--- src/app/dashboard/location/[id]/members/[mid]/components/MemberSubs/SubActions/ActionMenu.tsx
+++ src/app/dashboard/location/[id]/members/[mid]/components/MemberSubs/SubActions/ActionMenu.tsx
@@ -1,88 +1,109 @@
-'use client'
+"use client";
 import {
-    Button, Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle,
-    DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger
-} from '@/components/ui';
-import { Pencil, Trash2, X, Ellipsis } from 'lucide-react';
-import { MemberSubscription } from '@/types';
-import { CancelSub, UpdateSub } from '.';
-import { useState } from 'react';
-import { cn } from '@/libs/utils';
-import { VisuallyHidden } from 'react-aria';
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui";
+import { Pencil, Trash2, X, Ellipsis } from "lucide-react";
+import { MemberSubscription } from "@/types";
+import { CancelSub, UpdateSub } from ".";
+import { useState } from "react";
+import { cn } from "@/libs/utils";
+import { VisuallyHidden } from "react-aria";
 
-
-const HoverTransition = 'group-hover:bg-foreground/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300'
-
+const HoverTransition =
+  "group-hover:bg-foreground/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300";
 
 export function SubActions({ sub }: { sub: MemberSubscription }) {
+  const [action, setAction] = useState<"cancel" | "update" | undefined>(
+    undefined
+  );
 
-    const [action, setAction] = useState<'cancel' | 'update' | undefined>(undefined);
-
-    function handleClose(open: boolean) {
-        if (!open) {
-            setAction(undefined);
-        }
+  function handleClose(open: boolean) {
+    if (!open) {
+      setAction(undefined);
     }
+  }
 
-    return (
-        <>
-            <Dialog open={action !== undefined} onOpenChange={handleClose}>
-                <DialogContent className="max-w-lg border-foreground/10 sm:rounded-lg overflow-hidden">
-                    <VisuallyHidden className='pb-0 pt-5'>
-                        <DialogTitle className='text-sm'>
+  return (
+    <>
+      <Dialog open={action !== undefined} onOpenChange={handleClose}>
+        <DialogContent className="max-w-lg border-foreground/10 sm:rounded-lg overflow-hidden">
+          <VisuallyHidden className="pb-0 pt-5">
+            <DialogTitle className="text-sm"></DialogTitle>
+          </VisuallyHidden>
+          <CancelSub
+            sub={sub}
+            show={action === "cancel"}
+            close={() => handleClose(false)}
+          />
+          <UpdateSub
+            sub={sub}
+            show={action === "update"}
+            close={() => handleClose(false)}
+          />
+        </DialogContent>
+      </Dialog>
+      <div className="flex flex-row items-center ">
+        <Button
+          variant="ghost"
+          size="icon"
+          className={cn("size-7 flex-1  rounded-r-none", HoverTransition)}
+          onClick={() => setAction("update")}
+        >
+          <Pencil className="size-3" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className={cn(
+            "size-7 border-x rounded-none border-foreground/5 flex-1",
+            HoverTransition
+          )}
+          onClick={() => setAction("cancel")}
+        >
+          <X className="size-3.5" />
+        </Button>
 
-                        </DialogTitle>
-                    </VisuallyHidden>
-                    <CancelSub sub={sub} show={action === 'cancel'} close={() => handleClose(false)} />
-                    <UpdateSub sub={sub} show={action === 'update'} close={() => handleClose(false)} />
-                </DialogContent>
-
-            </Dialog>
-            <div className='flex flex-row items-center '>
-                <Button variant="ghost" size="icon"
-                    className={cn('size-7 flex-1  rounded-r-none', HoverTransition)}
-                    onClick={() => setAction('update')}>
-                    <Pencil className='size-3' />
-                </Button>
-                <Button variant="ghost" size="icon"
-                    className={cn('size-7 border-x rounded-none border-foreground/5 flex-1', HoverTransition)}
-                    onClick={() => setAction('cancel')}>
-                    <X className='size-3.5' />
-                </Button>
-
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon" className='size-7 group-hover:bg-foreground/5 flex-1 rounded-l-none'>
-                            <Ellipsis className="size-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className='border-foreground/10 '>
-
-                        <DropdownMenuItem
-                            className='cursor-pointer text-sm flex flex-row items-center justify-between gap-2'
-                            onClick={() => setAction('update')}
-                            disabled={!sub}
-                        >
-                            <span className='text-xs'> Update</span>
-                            <Pencil className='size-3' />
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem
-                            className='cursor-pointer text-sm flex flex-row items-center justify-between gap-2'
-                            onClick={() => setAction('cancel')}
-                            disabled={!sub}
-                        >
-                            <span className='text-xs'> Cancel</span>
-                            <Trash2 className='size-3' />
-                        </DropdownMenuItem>
-
-                    </DropdownMenuContent>
-                </DropdownMenu>
-
-            </div>
-        </>
-    )
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="size-7 group-hover:bg-foreground/5 flex-1 rounded-l-none"
+            >
+              <Ellipsis className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="border-foreground/10 ">
+            <DropdownMenuItem
+              className="cursor-pointer text-sm flex flex-row items-center justify-between gap-2"
+              onClick={() => setAction("update")}
+              disabled={!sub}
+            >
+              <span className="text-xs"> Update</span>
+              <Pencil className="size-3" />
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer text-sm flex flex-row items-center justify-between gap-2"
+              onClick={() => setAction("cancel")}
+              disabled={!sub}
+            >
+              <span className="text-xs"> Cancel</span>
+              <Trash2 className="size-3" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
+  );
 }
-
-
-
