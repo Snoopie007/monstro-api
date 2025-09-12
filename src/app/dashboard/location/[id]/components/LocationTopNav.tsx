30adaacfe01fd63c1f5a4b5bd8d470cae7698f53
cosmiclasagnadev <allenpdl75@gmail.com>
2025-09-12 22:57:25 +0800

    add 404 page

--- src/app/dashboard/location/[id]/components/LocationTopNav.tsx
+++ src/app/dashboard/location/[id]/components/LocationTopNav.tsx
@@ -1,37 +1,35 @@
-'use client'
-import { cn } from '@/libs/utils';
-import Image from 'next/image';
+"use client";
+import { cn } from "@/libs/utils";
+import Image from "next/image";
 
-import LocationSelect from './SelectLocation';
-import { AlertMenu, SupportMenu, UserMenu } from '@/components/navs';
+import LocationSelect from "./SelectLocation";
+import { AlertMenu, SupportMenu, UserMenu } from "@/components/navs";
 
-import React from 'react';
-import { usePathname } from 'next/navigation';
+import React from "react";
+import { usePathname } from "next/navigation";
 
+export function LocationTopNav({ lid }: { lid?: string }) {
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/").filter(Boolean);
 
-export function LocationTopNav({ lid }: { lid: string }) {
-    const pathname = usePathname();
-    const pathSegments = pathname.split('/').filter(Boolean);
-
-
-    return (
-        <div className=" w-full border-b border-foreground/10 py-2 px-3 flex flex-initial justify-between">
-
-
-            <div className='flex flex-row items-center gap-2'>
-                <div className={cn('logo  flex flex-row ')}>
-                    <Image src='/images/monstro-icon.webp' alt='' width={24} height={24} />
-                </div>
-                <LocationSelect locationId={lid} />
-
-
-            </div>
-            <div className='flex flex-row items-center gap-2'>
-
-                <AlertMenu />
-                <SupportMenu />
-                <UserMenu />
-            </div>
+  return (
+    <div className=" w-full border-b border-foreground/10 py-2 px-3 flex flex-initial justify-between">
+      <div className="flex flex-row items-center gap-2">
+        <div className={cn("logo  flex flex-row ")}>
+          <Image
+            src="/images/monstro-icon.webp"
+            alt=""
+            width={24}
+            height={24}
+          />
         </div>
-    )
+        {lid && <LocationSelect locationId={lid} />}
+      </div>
+      <div className="flex flex-row items-center gap-2">
+        <AlertMenu />
+        <SupportMenu />
+        <UserMenu />
+      </div>
+    </div>
+  );
 }
