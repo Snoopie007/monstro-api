b2980b53098c76e98741fba30a446bfa60be0ee0
cosmiclasagnadev <allenpdl75@gmail.com>
2025-09-20 04:25:49 +0800

    add markdown to message

--- src/app/dashboard/location/[id]/support/settings/components/TestChatBox/ChatMessage.tsx
+++ src/app/dashboard/location/[id]/support/settings/components/TestChatBox/ChatMessage.tsx
@@ -1,15 +1,15 @@
 'use client'
-import { cn } from "@/libs/utils";
-import { useEffect, useMemo, useRef } from "react";
-import { ScrollArea } from "@/components/ui";
+import { cn } from '@/libs/utils'
+import ReactMarkdown from 'react-markdown'
+import { useEffect, useMemo, useRef } from 'react'
+import { ScrollArea } from '@/components/ui'
 
-import { TestChatMessage } from "@/types";
-import { Placeholder } from "../../../components";
-import { useBotSettingContext } from "../../provider";
+import { TestChatMessage } from '@/types'
+import { Placeholder } from '../../../components'
+import { useBotSettingContext } from '../../provider'
 
 export function TestChatMessages() {
-    const { messages } = useBotSettingContext();
-
+    const { messages } = useBotSettingContext()
 
     const scrollRef = useRef<HTMLDivElement>(null)
 
@@ -20,7 +20,7 @@ export function TestChatMessages() {
     }, [messages])
 
     return (
-        <ScrollArea className='flex-1  max-h-[calc(100%-160px)]  '>
+        <ScrollArea className="flex-1  max-h-[calc(100%-160px)]  ">
             <div className="flex flex-col gap-4 p-4">
                 {messages.map((message: TestChatMessage) => (
                     <MessageFormat message={message} key={message.id} />
@@ -31,13 +31,11 @@ export function TestChatMessages() {
     )
 }
 
-
 function MessageFormat({ message }: { message?: TestChatMessage }) {
-
     const isAgent = useMemo(() => {
-        return message?.role === "assistant"
+        return message?.role === 'assistant'
     }, [message])
-
+    console.log(message?.content)
     return (
         <div className={cn(`message mb-3 break-words`)}>
             <div className="flex gap-4 flex-row justify-start">
@@ -46,17 +44,19 @@ function MessageFormat({ message }: { message?: TestChatMessage }) {
                 </div>
                 <div className="flex-1">
                     <span className="font-semibold leading-none">
-                        {isAgent ? "Monstro" : "You"}
+                        {isAgent ? 'Monstro' : 'You'}
                     </span>
-                    {message?.isLoading ? <Placeholder /> : (
-                        <div
-                            className="text-base text-foreground py-2 prose prose-sm max-w-none"
-                            dangerouslySetInnerHTML={{ __html: message?.content || '' }}
-                        />
+                    {message?.isLoading ? (
+                        <Placeholder />
+                    ) : (
+                        <div className="text-base text-foreground py-2 prose prose-sm max-w-none">
+                            <ReactMarkdown>
+                                {message?.content || ''}
+                            </ReactMarkdown>
+                        </div>
                     )}
                 </div>
             </div>
         </div>
     )
 }
-
