9021a8b8893ad0832c6cd69cc40034efed6f0f14
Snoopie007 <steve00006@gmail.com>
2025-10-31 11:21:05 -0400

    update

--- src/app/dashboard/location/[id]/members/[mid]/components/MemberAttendance/MonthView.tsx
+++ src/app/dashboard/location/[id]/members/[mid]/components/MemberAttendance/MonthView.tsx
@@ -20,7 +20,7 @@ export const MonthView = ({ weeks, month }: { weeks: any[], month: string }) => 
                             <div key={dayIndex} className="flex-1 aspect-square" >
                                 {day.isEmpty ? (
                                     <div className={cn(
-                                        'w-full h-full rounded-sm dark:border-slate-800 transition-colors duration-200 ',
+                                        'size-full rounded-sm dark:border-slate-800 transition-colors duration-200 ',
                                         'dark:bg-slate-900/20 bg-transparent'
                                     )} />
                                 ) : (
@@ -28,7 +28,9 @@ export const MonthView = ({ weeks, month }: { weeks: any[], month: string }) => 
                                         <HoverCardTrigger asChild>
                                             <div
                                                 className={cn(
-                                                    'w-full h-full rounded-sm dark:border-slate-800 cursor-pointer transition-colors duration-200 border border-gray-200 hover:border-gray-400',
+                                                    'size-full rounded-sm dark:border-slate-800 cursor-pointer',
+                                                    'transition-colors duration-200 border border-gray-200',
+                                                    'hover:border-gray-400',
                                                     day.count === 0 &&
                                                     'dark:bg-[#0f172a] bg-[#ebedf0]',
                                                     day.count > 0 &&
@@ -40,7 +42,9 @@ export const MonthView = ({ weeks, month }: { weeks: any[], month: string }) => 
                                             <div className="text-sm text-muted-foreground">
                                                 {formatDate(day.date, 'PPP')}
                                             </div>
-                                            <Separator className="mt-1 mb-2  bg-foreground/10" />
+                                            {day.attendances.length > 0 && (
+                                                <Separator className="mt-1 mb-2  bg-foreground/10" />
+                                            )}
                                             {day.attendances.map((attendance: Attendance, index: number) => (
                                                 <div key={index} className="flex flex-col gap-1 text-xs">
                                                     <div className=" flex flex-row gap-1 items-center justify-between " >
