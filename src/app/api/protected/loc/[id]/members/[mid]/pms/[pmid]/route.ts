d9a9c75d8bd33414e94bcac6914c2e145402abfc
Snoopie007 <steve00006@gmail.com>
2025-11-12 21:58:29 -0500

    update

---
+++ src/app/api/protected/loc/[id]/members/[mid]/pms/[pmid]/route.ts
@@ -0,0 +1,96 @@
+
+import { db } from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+import { MemberStripePayments } from "@/libs/server/stripe";
+import { memberPaymentMethods } from "@/db/schemas";
+
+type Props = {
+    params: Promise<{ id: string; mid: string; pmid: string }>
+}
+
+export async function PATCH(
+    req: Request,
+    props: Props
+) {
+    const { id, mid, pmid } = await props.params;
+    const { currentDefaultId } = await req.json();
+    try {
+
+        const mpm = await db.query.memberPaymentMethods.findFirst({
+            where: (memberPaymentMethods, { eq, and }) => and(
+                eq(memberPaymentMethods.paymentMethodId, pmid),
+                eq(memberPaymentMethods.memberId, mid),
+                eq(memberPaymentMethods.locationId, id)
+            ),
+            with: {
+                member: true,
+                paymentMethod: true,
+            },
+        });
+
+        if (mpm && mpm.isDefault) {
+            return NextResponse.json({ error: "Payment method is already the default" }, { status: 400 });
+        }
+        const integrations = await db.query.integrations.findFirst({
+            where: (integration, { eq }) =>
+                and(
+                    eq(integration.locationId, id),
+                    eq(integration.service, "stripe")
+                )
+        });
+        if (!integrations || !integrations.accountId) {
+            return NextResponse.json({ error: "Integration not found" }, { status: 404 });
+        }
+
+        if (!mpm?.member?.stripeCustomerId) {
+            return NextResponse.json({ error: "Member does not have a Stripe customer ID" }, { status: 404 });
+        }
+        // Set the new payment method as default
+        await db.update(memberPaymentMethods).set({
+            isDefault: true,
+        }).where(and(
+            eq(memberPaymentMethods.paymentMethodId, pmid),
+            eq(memberPaymentMethods.memberId, mid),
+            eq(memberPaymentMethods.locationId, id)
+        ));
+        // Set the previous default payment method as not default
+        await db.update(memberPaymentMethods).set({
+            isDefault: false,
+        }).where(and(
+            eq(memberPaymentMethods.paymentMethodId, currentDefaultId),
+            eq(memberPaymentMethods.memberId, mid),
+            eq(memberPaymentMethods.locationId, id)
+        ));
+
+        const stripe = new MemberStripePayments(integrations?.accountId);
+        stripe.setCustomer(mpm.member.stripeCustomerId);
+        await stripe.updateCustomer({
+            invoice_settings: {
+                default_payment_method: mpm.paymentMethod.stripeId,
+            },
+        });
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: err }, { status: 500 });
+    }
+}
+export async function DELETE(req: NextRequest, props: Props
+) {
+    const { id, mid, pmid } = await props.params;
+    try {
+
+        await db.delete(memberPaymentMethods).where(and(
+            eq(memberPaymentMethods.paymentMethodId, pmid),
+            eq(memberPaymentMethods.memberId, mid),
+            eq(memberPaymentMethods.locationId, id)
+        ));
+        return NextResponse.json(
+            { success: true },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: err }, { status: 500 });
+    }
+}
--- src/app/dashboard/location/[id]/members/[mid]/components/PaymentMethods/actions.tsx
+++ src/app/dashboard/location/[id]/members/[mid]/components/PaymentMethods/actions.tsx
@@ -1,77 +1,89 @@
 import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  Button,
-  DropdownMenuSeparator,
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	Button,
+	DropdownMenuSeparator,
 } from "@/components/ui";
 import { tryCatch } from "@/libs/utils";
 import { PaymentMethod } from "@/types";
 import { EllipsisVertical } from "lucide-react";
+import { useMemberStatus } from "../../providers";
+import { toast } from "sonner";
 
 interface PaymentMethodActionsProps {
-  paymentMethod: PaymentMethod;
-  mid: string;
-  lid: string;
+	paymentMethod: PaymentMethod;
+	mid: string;
+	lid: string;
 }
 
 export default function PaymentMethodsActions({
-  paymentMethod,
-  mid,
-  lid,
+	paymentMethod,
+	mid,
+	lid,
 }: PaymentMethodActionsProps) {
+	const { paymentMethods, setPaymentMethods } = useMemberStatus();
 
 
+	const PATH = `/api/protected/loc/${lid}/members/${mid}/pms`;
+	async function detach(id: string) {
+		const { result, error } = await tryCatch(fetch(`${PATH}/${id}`, { method: "DELETE" }));
+		if (error || !result || !result.ok) {
+			toast.error(error?.message ?? "Failed to detach payment method");
+			return;
+		}
+		setPaymentMethods((prev) => prev.filter((pm) => pm.id !== id));
+	}
 
-  async function detach(id: string) {
-    const { result, error } = await tryCatch(
-      fetch(`/api/protected/loc/${lid}/members/${mid}/pms/${id}`, {
-        method: "DELETE",
-      })
-    );
-  }
+	async function setDefault(id: string) {
 
-  async function setDefault(id: string) {
-    const { result, error } = await tryCatch(
-      fetch(`/api/protected/loc/${lid}/members/${mid}/pms/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-          isDefault: true,
-        }),
-      })
-    );
-  }
+		const currentDefault = paymentMethods.find((pm) => pm.isDefault);
+		const { result, error } = await tryCatch(fetch(`${PATH}/${id}`, {
+			method: "PATCH",
+			body: JSON.stringify({ currentDefaultId: currentDefault?.id })
+		}));
+		if (error || !result || !result.ok) {
+			toast.error(error?.message ?? "Failed to set default payment method");
+			return;
+		}
+		setPaymentMethods((prev) => prev.map((pm) => {
+			if (pm.id === id) {
+				return { ...pm, isDefault: true };
+			}
+			return pm;
+		}));
+	}
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant={"ghost"}
-          className="h-auto py-0 px-0 hover:bg-transparent"
-        >
-          <EllipsisVertical size={16} />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[180px] border-foreground/20 p-2">
-        {paymentMethod && (
-          <>
-            <DropdownMenuItem
-              className="cursor-pointer hover:bg-indigo-500 text-sm py-2 leading-5"
-              onClick={() => setDefault(paymentMethod.stripeId)}
-            >
-              <span>Default</span>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator className="mb-2" />
-            <DropdownMenuItem
-              className="cursor-pointer bg-red-500 "
-              onClick={() => detach(paymentMethod.stripeId)}
-            >
-              <span>Remove</span>
-            </DropdownMenuItem>
-          </>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant={"ghost"}
+					className="h-auto py-0 px-0 hover:bg-transparent"
+				>
+					<EllipsisVertical size={16} />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-[180px] border-foreground/20 p-2">
+				{paymentMethod && (
+					<>
+						<DropdownMenuItem
+							className="cursor-pointer hover:bg-indigo-500 text-sm py-2 leading-5"
+							onClick={() => setDefault(paymentMethod.id)}
+						>
+							<span>Default</span>
+						</DropdownMenuItem>
+						<DropdownMenuSeparator className="mb-2" />
+						<DropdownMenuItem
+							className="cursor-pointer bg-red-500 "
+							onClick={() => detach(paymentMethod.id)}
+						>
+							<span>Remove</span>
+						</DropdownMenuItem>
+					</>
+				)}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
