7fe9df142fa8374ba171642087d6f28d1dc61d22
cosmiclasagnadev <allenpdl75@gmail.com>
2025-11-19 07:19:08 +0800

    fix for multi session

--- src/libs/auth/client.ts
+++ src/libs/auth/client.ts
@@ -1,7 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+import { multiSessionClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  plugins: [
+    multiSessionClient()
+  ]
 });
 
-export const { useSession, signIn, signOut } = authClient;
+export const { useSession, signIn, signOut, multiSession } = authClient;
