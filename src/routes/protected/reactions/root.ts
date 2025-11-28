885ec9e52971309203dfb1655b76329777cb03da
Snoopie007 <steve00006@gmail.com>
2025-11-28 11:12:37 -0500

    update

--- src/routes/protected/reactions/root.ts
+++ src/routes/protected/reactions/root.ts
@@ -2,35 +2,45 @@ import { db } from "@/db/db";
 import { reactions } from "@/db/schemas/chat";
 import Elysia from "elysia";
 import type { ReactionEmoji } from "@/types";
+import { and, eq } from "drizzle-orm";
 
 type ReactionProps = {
     memberId: string
     userId: string
     params: {
+        ownerType: string
         ownerId: string
     }
     status: any
 }
 
 type PostBody = {
-    emoji: ReactionEmoji,
-    ownerType: string,
+    display: string,
+    type: string,
+    name: string,
     userId: string,
     userName: string,
 }
-export const reactionRoutes = new Elysia({ prefix: '/reactions/:ownerId' })
+export const reactionRoutes = new Elysia({ prefix: '/reactions/:ownerType/:ownerId' })
     .post('/', async ({ status, params, body }: ReactionProps & { body: PostBody }) => {
-        const { ownerId } = params;
-        const { emoji, ownerType, userId, userName } = body;
-        if (!userName || !userId || !ownerType || !emoji) {
+        const { ownerId, ownerType } = params;
+        const { display, type, name, userId, userName } = body;
+
+        if (!['message', 'post', 'moment'].includes(ownerType)) {
+            return status(400, { error: 'Invalid owner type' });
+        }
+
+        if (!userId || !display || !type || !name || !userName) {
             return status(400, { error: 'All fields are required' });
         }
+
         const today = Date.now();
         try {
 
-            const newEmoji = {
-                ...emoji,
-                type: emoji.type ? emoji.type.toLowerCase() : 'unicode',
+            const newEmoji: ReactionEmoji = {
+                value: display,
+                type: type ? type.toLowerCase() : 'unicode',
+                name: name,
             }
             await db.insert(reactions).values({
                 ownerId,
@@ -40,7 +50,7 @@ export const reactionRoutes = new Elysia({ prefix: '/reactions/:ownerId' })
             });
             return status(200, {
                 ...newEmoji,
-                ownerType: ownerType.toLowerCase(),
+                ownerType,
                 userId,
                 ownerId,
                 userIds: [userId],
@@ -48,6 +58,21 @@ export const reactionRoutes = new Elysia({ prefix: '/reactions/:ownerId' })
                 userNames: [userName],
                 created: today,
             });
+        } catch (error) {
+            console.error(error);
+            return status(500, { error: 'Internal server error' });
+        }
+    }).delete('/', async ({ params, status }: ReactionProps) => {
+        const { ownerId, ownerType } = params;
+        if (!['message', 'post', 'moment'].includes(ownerType)) {
+            return status(400, { error: 'Invalid owner type' });
+        }
+        try {
+            await db.delete(reactions).where(and(
+                eq(reactions.ownerId, ownerId),
+                eq(reactions.ownerType, ownerType)
+            ));
+            return status(200, { message: 'Reaction deleted' });
         } catch (error) {
             console.error(error);
             return status(500, { error: 'Internal server error' });
