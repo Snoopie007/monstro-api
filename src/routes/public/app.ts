79824a2a587e647343fb3703cb131cb2d06fc80e
Snoopie007 <steve00006@gmail.com>
2026-01-26 22:06:26 -0500

    update

--- src/routes/public/app.ts
+++ src/routes/public/app.ts
@@ -1,11 +1,17 @@
 import { Elysia } from "elysia"
 
 
+const CURRENT_VERSION = `1.0.95`;
+const PREVIOUS_VERSION = `1.0.92`;
+const MINIMUM_VERSION = `1.0.95`;
+
 export const appStatsRoutes = new Elysia({ prefix: '/app' })
     .get('/version', async ({ params, status }) => {
         return status(200, {
-            minimumVersion: "1.0.91",
-            latestVersion: "1.0.91",
+            previousVersion: PREVIOUS_VERSION,
+            minimumVersion: MINIMUM_VERSION,
+            latestVersion: CURRENT_VERSION,
+            forceUpdateRequired: false,
         });
     });
 
