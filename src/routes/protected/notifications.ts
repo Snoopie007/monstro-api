5b215701bc08ff86f3da772bf51eb0fa9e3f154c
Snoopie007 <steve00006@gmail.com>
2026-02-02 12:14:45 -0500

    update

--- src/routes/protected/notifications.ts
+++ src/routes/protected/notifications.ts
@@ -3,17 +3,9 @@ import type { Elysia, Context } from "elysia";
 import { z } from "zod";
 import { userNotifications } from "@/db/schemas";
 
-const MobileNotificationSchema = {
-    body: z.object({
-        token: z.string(),
-        deviceModelId: z.string().nullish(),
-        deviceName: z.string(),
-        platform: z.enum(["ios", "android"]),
-    }),
-};
 
 export async function userNotificationRoutes(app: Elysia) {
-    app.post('/notification/token', async ({ body, status, ...ctx }) => {
+    app.post('/notification/enable', async ({ body, status, ...ctx }) => {
         const { userId } = ctx as Context & { userId: string };
         const data = body;
         if (!data.token) {
@@ -24,13 +16,24 @@ export async function userNotificationRoutes(app: Elysia) {
             await db.insert(userNotifications).values({
                 userId,
                 ...data,
+                enabled: true,
                 created: today,
+            }).onConflictDoNothing({
+                target: [userNotifications.token],
             });
             return status(200, { success: true });
         } catch (error) {
             console.error(error);
             return status(500, { message: "Internal server error" });
         }
-    }, MobileNotificationSchema);
+    }, {
+        body: z.object({
+            token: z.string(),
+            deviceModelId: z.string().nullish(),
+            deviceName: z.string(),
+            platform: z.enum(["ios", "android"]),
+        }),
+    });
+
     return app;
 }
