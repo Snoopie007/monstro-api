c4d732dc51c963ed7ac5c651c8a14fb4e7a10682
Snoopie007 <steve00006@gmail.com>
2026-02-02 19:25:47 -0500

    update

--- src/libs/schemas.ts
+++ src/libs/schemas.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const AuthAdditionalDataSchema = z.object({
-    migrateId: z.string().optional(),
-    ref: z.string().optional(),
-    lid: z.string().optional(),
-    familyId: z.string().optional(),
+    migrateId: z.string().nullish().optional(),
+    ref: z.string().nullish().optional(),
+    lid: z.string().nullish().optional(),
+    familyId: z.string().nullish().optional(),
 });
