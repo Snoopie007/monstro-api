8214258ebe7adae41e1b7dafcae9467a1c64e682
Snoopie007 <steve00006@gmail.com>
2026-02-13 19:04:04 -0500

    udpate

--- types/chat.ts
+++ types/chat.ts
@@ -1,8 +1,9 @@
 import { chatMembers, chats, messages } from "../schemas/chat";
 import type { Group } from "./group";
-import type { Media } from "./medias";
-import type { User } from "./user";
 import type { Location } from "./location";
+import type { Media } from "./medias";
+import type { ReactionCounts } from "./reactions";
+import type { User } from "./user";
 type LastChatMessage = {
     content: string | null;
     created: Date;
@@ -15,11 +16,14 @@ export type Chat = typeof chats.$inferSelect & {
     location?: Location;
     chatMembers?: ChatMember[];
     lastMessage?: LastChatMessage;
+
 }
 export type Message = typeof messages.$inferSelect & {
     medias?: Media[];
     reply?: MessageReply;
     sender?: MessageSender;
+    reactions?: ReactionCounts[];
+    files?: Media[];
 }
 
 export type MessageSender =
