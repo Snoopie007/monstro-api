14fd47b0bca24449d538f16372c78b017db4f352
cosmiclasagnadev <allenpdl75@gmail.com>
2026-02-24 02:09:40 +0800

    Merge commit '20a8e94c14f74f1b8f1071267814641ee68d06a8'

--- subtrees/schemas/invoice.ts
+++ subtrees/schemas/invoice.ts
@@ -3,28 +3,27 @@ import { boolean, integer, jsonb, pgTable, text, timestamp, uuid } from "drizzle
 import type { InvoiceItem } from "../types/invoices";
 import { InvoiceStatusEnum, PaymentTypeEnum } from "./DatabaseEnums";
 import { locations } from "./locations";
-import { memberSubscriptions } from "./MemberPlans";
 import { members } from "./members";
 
 export const memberInvoices = pgTable('member_invoices', {
     id: uuid('id').primaryKey().notNull().default(sql`uuid_base62()`),
     metadata: jsonb('metadata').$type<Record<string, any>>().default(sql`'{}'::jsonb`),
-    currency: text('currency'),
+    currency: text('currency').notNull().default('usd'),
     memberId: text('member_id').notNull().references(() => members.id, { onDelete: 'cascade' }),
     locationId: text('location_id').notNull().references(() => locations.id, { onDelete: 'cascade' }),
-    memberSubscriptionId: text('member_subscription_id').references(() => memberSubscriptions.id, { onDelete: 'cascade' }),
+    memberPlanId: text('member_plan_id'),
     description: text('description'),
     items: jsonb('items').array().$type<InvoiceItem[]>().default(sql`'{}'::jsonb[]`),
     paid: boolean('paid').notNull().default(false),
     tax: integer('tax').notNull().default(0),
     total: integer('total').notNull().default(0),
-    discount: integer('discount').notNull().default(0),
     subTotal: integer('subtotal').notNull().default(0),
     forPeriodStart: timestamp('for_period_start', { withTimezone: true }),
     forPeriodEnd: timestamp('for_period_end', { withTimezone: true }),
     dueDate: timestamp('due_date', { withTimezone: true }).notNull().defaultNow(),
     attemptCount: integer('attempt_count').notNull().default(0),
     invoicePdf: text('invoice_pdf'),
+    stripeReceiptUrl: text('stripe_receipt_url'),
     status: InvoiceStatusEnum('status').notNull().default('draft'),
     paymentType: PaymentTypeEnum('payment_type').notNull().default('cash'),
     invoiceType: text('invoice_type').notNull().default('one-off'),
