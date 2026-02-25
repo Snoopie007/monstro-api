5cd7ff105724c3d226689ef2dcee2faf3ee2ebc4
Snoopie007 <steve00006@gmail.com>
2026-02-24 22:32:31 -0500

    updates

--- schemas/chat/chats.ts
+++ schemas/chat/chats.ts
@@ -27,9 +27,9 @@ export const chats = pgTable("chats", {
 export const chatMembers = pgTable("chat_members", {
     chatId: text("chat_id").notNull().references(() => chats.id, { onDelete: "cascade" }),
     userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
-    last_message_id: text("last_message_id").references(() => messages.id, { onDelete: "set null" }),
-    last_active_at: timestamp("last_active_at", { withTimezone: true }).notNull().defaultNow(),
-    unread_count: integer("unread_count").notNull().default(0),
+    lastMessageId: text("last_message_id").references(() => messages.id, { onDelete: "set null" }),
+    lastActiveAt: timestamp("last_active_at", { withTimezone: true }).notNull().defaultNow(),
+    unreadCount: integer("unread_count").notNull().default(0),
     joined: timestamp("joined_at", { withTimezone: true }).notNull().defaultNow(),
 }, (t) => [
     primaryKey({ columns: [t.chatId, t.userId] }),
