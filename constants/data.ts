1c811ffb69ccda058f351d00c1f439ff9c73293e
Snoopie007 <steve00006@gmail.com>
2026-02-27 12:59:56 -0500

    update real time feeds and chat

--- constants/data.ts
+++ constants/data.ts
@@ -225,6 +225,12 @@ const RealTimeEvents = {
         NEW_FEED: "feed:new",
         UPDATED_FEED: "feed:updated",
         DELETED_FEED: "feed:deleted",
+    },
+    support: {
+        NEW_SUPPORT_MESSAGE: "system:message",
+        UPDATED_SUPPORT_MESSAGE: "new:message",
+        UPDATED: "updated:support",
+        NEW: "new:support",
     }
 }
 
