50683be1dcafc56c94bad33d50b7003dbc42b8f7
Snoopie007 <steve00006@gmail.com>
2026-03-17 16:14:39 -0400

    major update

--- types/MemberPass.ts
+++ types/MemberPass.ts
@@ -2,8 +2,8 @@ import { memberPasses } from "../schemas/MemberPasses";
 import { Location } from "./location";
 import { Member, MemberPlan } from "./member";
 export type MemberPass = typeof memberPasses.$inferSelect & {
-    referrer: Member;
-    location: Location;
-    claimedByMember: Member;
-    plan: MemberPlan;
+    referrer?: Member;
+    location?: Location;
+    claimedByMember?: Member;
+    plan?: MemberPlan;
 }
