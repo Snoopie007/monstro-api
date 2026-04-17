38ce71c9189d9ab4c2011266e58b6cd2eb2386d3
Snoopie007 <steve00006@gmail.com>
2026-04-17 14:50:21 -0400

    update payment methods

--- schemas/relations.ts
+++ schemas/relations.ts
@@ -22,7 +22,6 @@ import {
 } from "./members";
 import { memberPasses } from "./MemberPasses";
 import { migrateMembers } from "./MigrateMembers";
-import { memberPaymentMethods, paymentMethods } from "./PaymentMethods";
 import { planPrograms, programs, programSessions, sessionWaitlist } from "./programs";
 import { promos } from "./promos";
 import { reservationExceptions, reservations } from "./reservations";
@@ -330,10 +329,6 @@ export const memberLocationsRelations = relations(
 		attendances: many(attendances),
 		pointsHistory: many(memberPointsHistory, {
 			relationName: 'pointsHistory',
-		}),
-		memberPaymentMethods: many(memberPaymentMethods, {
-			relationName: 'memberPaymentMethods',
-
 		}),
 	})
 );
@@ -707,33 +702,6 @@ export const transactionsRelations = relations(transactions, ({ one }) => ({
 	}),
 }));
 
-// ============================================================================
-// PAYMENT METHOD RELATIONS
-// ============================================================================
-
-export const paymentMethodsRelations = relations(paymentMethods, ({ many }) => ({
-	memberPaymentMethods: many(memberPaymentMethods),
-}));
-
-export const memberPaymentMethodsRelations = relations(memberPaymentMethods, ({ one }) => ({
-	member: one(members, {
-		fields: [memberPaymentMethods.memberId],
-		references: [members.id],
-	}),
-	location: one(locations, {
-		fields: [memberPaymentMethods.locationId],
-		references: [locations.id],
-	}),
-	paymentMethod: one(paymentMethods, {
-		fields: [memberPaymentMethods.paymentMethodId],
-		references: [paymentMethods.id],
-	}),
-	memberLocation: one(memberLocations, {
-		fields: [memberPaymentMethods.memberId, memberPaymentMethods.locationId],
-		references: [memberLocations.memberId, memberLocations.locationId],
-		relationName: 'memberPaymentMethods',
-	}),
-}));
 
 // ============================================================================
 // INTEGRATION RELATIONS
