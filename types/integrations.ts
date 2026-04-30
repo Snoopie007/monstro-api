24095b721981656ff722ad3b8b4f73eaeb55baa5
Snoopie007 <steve00006@gmail.com>
2026-04-30 13:39:30 -0400

    update square payment

--- types/integrations.ts
+++ types/integrations.ts
@@ -1,7 +1,16 @@
 import { integrations } from "../schemas/integrations";
 import type { Location } from "./location";
 
+
+
+export type IntegrationMetadata = {
+
+    squareLocationId?: string;
+}
+
 export type Integration = typeof integrations.$inferSelect & {
     location?: Location;
+    metadata?: IntegrationMetadata;
 }
+
 
