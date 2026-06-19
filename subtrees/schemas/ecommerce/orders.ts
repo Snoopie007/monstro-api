e1dd217dbc2e2cf2e2071fb907745401138843d0
Snoopie007 <steve00006@gmail.com>
2026-06-18 20:33:37 -0400

    update fix

--- subtrees/schemas/ecommerce/orders.ts
+++ subtrees/schemas/ecommerce/orders.ts
@@ -12,7 +12,7 @@ import {
 
 import type { Address } from "../../types/other";
 import { members } from "../members";
-import type { OrderLineItem } from "../../types/mercs";
+import type { OrderLineItem } from "../../types/order";
 import { locations } from "../locations";
 
 export const orders = pgTable("orders", {
