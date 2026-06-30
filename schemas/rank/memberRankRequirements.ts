65d336064c697eb30341bbc3dfe1f2733ce210d7
Snoopie007 <steve00006@gmail.com>
2026-06-30 16:05:37 -0400

    fix

--- schemas/rank/memberRankRequirements.ts
+++ schemas/rank/memberRankRequirements.ts
@@ -1,6 +1,6 @@
 import { index, integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
 import { staffs } from "../staffs";
-import { rankRequirements } from "./RankRequirements";
+import { rankRequirements } from "./rankRequirements";
 import { locations } from "../locations";
 import { members } from "../members";
 
