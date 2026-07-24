6f49ee0588c834b4200373724f43e0d9bd2c3add
cosmiclasagnadev <allenpdl75@gmail.com>
2026-07-25 01:37:37 +0800

    Complete Authorize.net webhook payment handling

--- src/routes/webhooks/root.ts
+++ src/routes/webhooks/root.ts
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia";
 import { stripeWebhookRoutes } from "./stripe";
 import { squareWebhookRoutes } from "./square";
+import { authorizeWebhookRoutes } from "./authorize";
 
 export function webhooksRoutes(app: Elysia) {
     app.group("/webhooks", (app) => {
         app.use(stripeWebhookRoutes);
         app.use(squareWebhookRoutes);
+        app.use(authorizeWebhookRoutes);
         return app;
     });
     return app;
