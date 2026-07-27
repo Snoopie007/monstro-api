8d75dd9be1bfacda0b9c113e6d975b7968eb28bd
Snoopie007 <steve00006@gmail.com>
2026-07-27 15:46:18 -0400

    update

--- types/PaymentMethods.ts
+++ types/PaymentMethods.ts
@@ -4,7 +4,7 @@ import type { Address } from "./other"
 
 export type PaymentMethod = {
     id: string
-    source: 'stripe' | 'square'
+    source: 'stripe' | 'square' | 'authorizenet'
     type: PaymentType
     address?: Address
     card?: CardPaymentMethod
